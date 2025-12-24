af7e1342aeaf91ea96552d54e1771bd4114338d3
Yannick Herrero <yannick.herrero@proton.me>
2025-12-24 07:00:36 +0100

    refactor: migrate to native nvim 0.11 LSP API
    - remove nvim-lspconfig and mason-lspconfig dependencies
    - use vim.lsp.config() and vim.lsp.enable() instead
    - keep mason.nvim for server installation
    - fixes deprecation warning

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -1,46 +1,61 @@
 return {
+  -- Mason for installing LSP servers (run :Mason to install servers)
   {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
     end,
   },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "tailwindcss" },
-      })
-    end,
-  },
+  -- Fidget for LSP progress notifications
   {
     "j-hui/fidget.nvim",
     opts = {},
   },
+  -- cmp-nvim-lsp for enhanced completion capabilities
   {
-    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Configure LSP servers using vim.lsp.config (Neovim 0.11+)
-      vim.lsp.config("lua_ls", {
+      -- Global LSP settings (applies to all servers)
+      vim.lsp.config("*", {
         capabilities = capabilities,
-      })
-      vim.lsp.config("ts_ls", {
-        capabilities = capabilities,
-      })
-      vim.lsp.config("tailwindcss", {
-        capabilities = capabilities,
+        root_markers = { ".git" },
       })
 
-      -- Enable configured servers
+      -- Lua Language Server
+      vim.lsp.config("lua_ls", {
+        cmd = { "lua-language-server" },
+        filetypes = { "lua" },
+        root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", "stylua.toml", ".git" },
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+          },
+        },
+      })
+
+      -- TypeScript/JavaScript Language Server
+      vim.lsp.config("ts_ls", {
+        cmd = { "typescript-language-server", "--stdio" },
+        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+        root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+      })
+
+      -- Tailwind CSS Language Server
+      vim.lsp.config("tailwindcss", {
+        cmd = { "tailwindcss-language-server", "--stdio" },
+        filetypes = { "html", "css", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+        root_markers = { "tailwind.config.js", "tailwind.config.ts", "postcss.config.js", ".git" },
+      })
+
+      -- Enable the configured servers
       vim.lsp.enable({ "lua_ls", "ts_ls", "tailwindcss" })
 
+      -- Setup fidget
       require("fidget").setup()
-
-      -- Completion options
-      vim.o.completeopt = "menuone,noinsert,noselect"
-      vim.opt.shortmess = vim.opt.shortmess + "c"
 
       -- Diagnostics config
       vim.diagnostic.config({
@@ -60,12 +75,11 @@ return {
       vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
       vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
-      -- Keymaps
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      -- Additional keymaps (Neovim 0.11 already provides gra, grn, grr, gri, grt, gO, K)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+      vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename symbol" })
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
     end,
   },
 }
