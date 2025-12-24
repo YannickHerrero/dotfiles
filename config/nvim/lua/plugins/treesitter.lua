02f5605c3e5aa1f93603f3dc54792d885d9add58
Yannick Herrero <yannick.herrero@proton.me>
2025-12-24 07:04:07 +0100

    fix: update treesitter to modern lazy.nvim opts API

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,10 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = {"lua", "javascript"},
-        highlight = { enable = true },
-        indent = { enable = true },  
-      })
-    end
-  }
+    opts = {
+      ensure_installed = { "lua", "javascript" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+  },
 }
