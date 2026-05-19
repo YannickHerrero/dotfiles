5e44d023ae30eb88ac1ece18d9943d159bd5a7e1
Yannick Herrero <yannick.herrero@proton.me>
2026-05-19 08:59:11 +0000

    swap neo-tree for snacks.explorer
    Replaces neo-tree.nvim (+ nui.nvim + nvim-web-devicons) with
    snacks.explorer. Same sidebar UX, but it shares the snacks runtime
    already loaded for the picker, and pulls icons from mini.icons
    instead of nvim-web-devicons. Ports the existing <leader>e keymap.

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -4,11 +4,13 @@ return {
   lazy = false,
   opts = {
     picker = { enabled = true },
+    explorer = { enabled = true },
     bigfile = { enabled = true },
     quickfile = { enabled = true },
   },
   keys = {
     { "<leader> ", function() Snacks.picker.files() end, desc = "find file" },
     { "<leader>sg", function() Snacks.picker.grep() end, desc = "live grep" },
+    { "<leader>e", function() Snacks.explorer() end, desc = "file explorer" },
   },
 }
