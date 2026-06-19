e614a3f9c62d1f0c65a4833620eaaed1ede51b15
YannickHerrero <yannick.herrero@proton.me>
2026-06-19 16:31:17 +0200

    nvim: also accept Supermaven suggestion with <C-j>

--- config/nvim/lua/plugins/supermaven.lua
+++ config/nvim/lua/plugins/supermaven.lua
@@ -1,14 +1,20 @@
 return {
   "supermaven-inc/supermaven-nvim",
   event = "InsertEnter",
-  opts = {
-    keymaps = {
-      accept_suggestion = "<Tab>",
-      clear_suggestion = "<C-]>",
-      accept_word = "<C-j>",
-    },
-    -- Inline ghost-text suggestions; blink.cmp keeps handling the popup menu.
-    disable_inline_completion = false,
-    disable_keymaps = false,
-  },
+  config = function()
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<Tab>",
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-l>",
+      },
+      -- Inline ghost-text suggestions; blink.cmp keeps handling the popup menu.
+      disable_inline_completion = false,
+      disable_keymaps = false,
+    })
+
+    -- Accept the whole suggestion with <C-j> too, in addition to <Tab>.
+    local preview = require("supermaven-nvim.completion_preview")
+    vim.keymap.set("i", "<C-j>", preview.on_accept_suggestion, { desc = "Supermaven: accept suggestion" })
+  end,
 }
