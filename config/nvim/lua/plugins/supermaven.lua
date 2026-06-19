c52bc85099390638c2e211ac44d94d28f48659bb
YannickHerrero <yannick.herrero@proton.me>
2026-06-19 16:39:59 +0200

    nvim: use Tab/S-Tab for Supermaven, drop C-j/C-l
    C-j/C-l are captured by tmux for pane navigation and never reach nvim,
    so they could not accept suggestions. Tab accepts the full suggestion
    and S-Tab accepts a single word.

--- config/nvim/lua/plugins/supermaven.lua
+++ config/nvim/lua/plugins/supermaven.lua
@@ -1,20 +1,14 @@
 return {
   "supermaven-inc/supermaven-nvim",
   event = "InsertEnter",
-  config = function()
-    require("supermaven-nvim").setup({
-      keymaps = {
-        accept_suggestion = "<Tab>",
-        clear_suggestion = "<C-]>",
-        accept_word = "<C-l>",
-      },
-      -- Inline ghost-text suggestions; blink.cmp keeps handling the popup menu.
-      disable_inline_completion = false,
-      disable_keymaps = false,
-    })
-
-    -- Accept the whole suggestion with <C-j> too, in addition to <Tab>.
-    local preview = require("supermaven-nvim.completion_preview")
-    vim.keymap.set("i", "<C-j>", preview.on_accept_suggestion, { desc = "Supermaven: accept suggestion" })
-  end,
+  opts = {
+    keymaps = {
+      accept_suggestion = "<Tab>",
+      accept_word = "<S-Tab>",
+      clear_suggestion = "<C-]>",
+    },
+    -- Inline ghost-text suggestions; blink.cmp keeps handling the popup menu.
+    disable_inline_completion = false,
+    disable_keymaps = false,
+  },
 }
